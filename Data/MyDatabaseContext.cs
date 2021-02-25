b6ebabc1979070df2996f2cadb55fb0ebdfb9833
Marco Obinu <locurtola@tin.it>
2021-02-25 17:00:16 +0100

    AAD MSI Auth

--- Data/MyDatabaseContext.cs
+++ Data/MyDatabaseContext.cs
@@ -3,14 +3,23 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.EntityFrameworkCore;
+using Azure.Identity;
 
 namespace DotNetCoreSqlDb.Models
 {
     public class MyDatabaseContext : DbContext
     {
+        private static Boolean isInitialized;
+
         public MyDatabaseContext (DbContextOptions<MyDatabaseContext> options)
             : base(options)
         {
+            var conn = (Microsoft.Data.SqlClient.SqlConnection)Database.GetDbConnection();
+            var credential = new DefaultAzureCredential();
+            var token = credential
+                    .GetToken(new Azure.Core.TokenRequestContext(
+                        new[] { "https://database.windows.net/.default" }));
+            conn.AccessToken = token.Token;
         }
 
         public DbSet<DotNetCoreSqlDb.Models.Todo> Todo { get; set; }
